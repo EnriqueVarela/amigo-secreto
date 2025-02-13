08fa115d5c05c71f8f29104d6f0e3b4faacde7d5
kikex <evarela.herbage@gmail.com>
2025-02-13 16:43:15 -0300

    actualizando codigo

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -1,1 +1,31 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarTextoElemento(elemento,texto){
+    let elementoHTML = document.getElementById(elemento);
+    elementoHTML.innerHTML = texto;
+    return;
+}
+
+
+function agregarAmigo(){
+    let nombre = document.getElementById("amigo").value;
+    console.log(nombre);
+    console.log(nombre == "");
+    if (nombre == ""){
+        return alert("numero no valido");
+    } else{
+        amigos.push(nombre);
+        console.log(amigos);
+        agregarTextoElemento("listaAmigos", `${amigos}`);
+        limpiarCaja();
+    }
+    }
+    
+
+
+function limpiarCaja(){
+    agregarTextoElemento("amigo", "");
+}
+
+agregarAmigo();
